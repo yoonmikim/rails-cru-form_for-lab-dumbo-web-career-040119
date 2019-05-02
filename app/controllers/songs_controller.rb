69a3e3ee9e8e3f0640f4e20d0f3f9e450eebc380
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-05-02 14:51:37 +0000

    Done.

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,37 @@
+class SongsController < ApplicationController
+    before_action :get_song, only: [:show, :edit, :update]
+  
+  def index 
+    @songs = Song.all 
+  end
+  
+  def new 
+    @song = Song.new 
+  end
+  
+  def create
+    @song = Song.create(song_params) 
+    redirect_to @asong
+  end
+  
+  def update 
+    @song.update(song_params)
+    redirect_to @song
+  end
+  
+  def destroy 
+    Artist.destroy(params[:id])
+    redirect_to songs_path
+  end
+  
+  private 
+  
+  def get_song
+    @song = Song.find(params[:id])
+  end
+  
+  def song_params
+    params.require(:song).permit(:name, :artist_id, :genre_id)
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+artist1 = Artist.create(name: "Artist 1", bio: "Artist 1 bio")
+
+artist2 = Artist.create(name: "Artist 2", bio: "Artist 2 bio")
+
+artist3 = Artist.create(name: "Artist 3", bio: "Artist 3 bio")
+
+genre1 = Genre.create(name: "genre 1")
+
+genre2 = Genre.create(name: "genre 2")
+
+genre3 = Genre.create(name: "genre 3")
+
+song1 = Song.create(name: "Song 1", artist_id: artist1, genre_id: genre1)
+
+song2 = Song.create(name: "Song 2", artist_id: artist2, genre_id: genre2)
+
+song3 = Song.create(name: "Song 3", artist_id: artist3, genre_id: genre3)
